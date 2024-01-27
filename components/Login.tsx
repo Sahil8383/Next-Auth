08e8499b5c689ca00ea3602a4e870297055512f1
Sahil Bhatt <ethenhunt556@gmail.com>
2024-01-27 23:03:24 +0530

    integrated shadcn lib

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,27 @@
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
 
 
 const Login = () => {
   return (
-    <div>
-        <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Login</button>
-        </form>
+    <div
+      className="flex flex-col justify-center items-center w-full h-screen bg-gray-100"
+    >
+      <form
+        className="flex flex-col justify-center items-center bg-white rounded-md shadow-md p-8"
+      >
+        <Input
+          type="text"
+          placeholder="Username"
+          className="w-64"
+        />
+        <Input 
+          type="password" 
+          placeholder="Password" 
+          className="w-64 mt-2"
+        />
+        <Button type="submit">Login</Button>
+      </form>
     </div>
   )
 }
